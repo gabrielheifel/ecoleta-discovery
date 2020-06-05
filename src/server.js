e21f8f56570dd2924bd3927b71d46d3a2b7ee3ea
Gabriel Heifel <gabrielheifel@gmail.com>
2020-06-05 08:27:10 -0300

    end 4 class

--- src/server.js
+++ src/server.js
@@ -3,20 +3,38 @@ const server = express()
 
 // Configurar pasta public
 server.use(express.static("public"))
+
+
+// Utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true,      // Sem cache para não entregar resultados antigos
+})
+
  
 // Configurar caminhos da minha aplicação
-// Página inicial
+// Página inicial com renderizador (nunjucks)
 // req: Requisição
 // res: Resposta
 server.get("/", (req, res) => {
-    //Envida o html
-    res.sendFile(__dirname + "/views/index.html")   //__dirname = diretorio onde esta o server.js no caso    
+    //Envida o html dinâmico
+    return res.render("index.html", { title: "Um Título"})    
 })
 
 server.get("/create-point", (req, res) => {
-    //Envida o html
-    res.sendFile(__dirname + "/views/create-point.html")     
+    return res.render("create-point.html")
 })
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+// Configurar caminhos da minha aplicação
+// Sem renderizador (nunjucks)
+// server.get("/create-point", (req, res) => {
+//     //Envida o html
+//     return res.sendFile(__dirname + "/views/create-point.html")     //__dirname = diretorio onde esta o server.js no caso
+// })
 
 // Ligar o servidor
 server.listen(3000)     // Abre a porta localhost:3000 do servidor(no browser)
